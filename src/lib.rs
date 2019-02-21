1ca7124c95e999b68a9db3a4be70d35e378a7908
gasc <mayeul.gasc@orange.fr>
2019-02-21 16:33:44 +0100

    put in place the main functions and the trait scaffold

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
-extern crate embedded_hal as hal;
 
+use nb::block;
+use embedded_hal::digital::*;
+
+let duration = 0;
 
 enum Color {
     Red,
@@ -8,11 +11,35 @@ enum Color {
     Blue,
 }
 
-fn read_color(s0 : InputPint, s1 : InputPint, s2: InputPint, s3: InputPint, out : Hertz) -> Color {
+
+//Lit la durée de d'une période de valeur opposée
+fn pulseIn(SensorOut : u32, value : bool){
+    let time : u32 = 0; //Chronometre
+
+    //On attend qu'un signal arrive
+    while hal::digital::InputPin::is_high(SensorOut) {}
+
+    //On chronometre (voir hal::capture)
+
+
+    return ;
+}
+
+//Retourne la couleur majoritaire
+fn read_color(s0 : InputPint, s1 : InputPint, s2: InputPint, s3: InputPint, SensorOut : Hertz) -> Color {
+    let couleur : Color;
 
     //Initialisation
     hal::digital::OutputPin::set_high(s0);
     hal::digital::OutputPin::set_low(s1);
+
+    //Lecture
+    hal::digital::OutputPin::set_low(s2);
+    hal::digital::OutputPin::set_low(s3);
+
+    duration = pulseIn(SensorOut, false);
+
+    return couleur;
 }
 
 #[cfg(test)]
