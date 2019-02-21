d433c8cb4bf2c9df9fd71a908708f9074287c2ca
Paul Florence <perso@florencepaul.com>
2019-02-21 15:38:32 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+fn read_color(s0 : InputPint, s1 , s2, s3, out : Hertz) -> Color {
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
