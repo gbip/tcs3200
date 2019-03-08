905f3d2f55d79dab45e11e0d5b797da1d6494481
gasc <mayeul.gasc@orange.fr>
2019-03-08 17:06:18 +0100

    Removing warning

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 //use embedded_hal::digital::*;
-use embedded_hal::prelude::*;
+//use embedded_hal::prelude::*;
 use embedded_hal::digital::OutputPin;
 //use nb::*;
 
@@ -33,36 +33,36 @@ use embedded_hal::digital::OutputPin;
 //
 
 ///Contient les 3 composantes d'une couleur échantillonées de 0 (noir) à 255 (blanc)
-pub struct Color {
-    pub Red : u32, // de 0 à 255
-    pub Green : u32,
-    pub Blue : u32,
+pub struct color {
+    pub red : u32, // de 0 à 255
+    pub green : u32,
+    pub blue : u32,
 }
 
 pub struct Hertz(u32);
 
 
 //La fonction hzToColor reçoit en arguments :
-//      fnRead --> Fonction de lecture de la fréquence (retourne des Hertz)
+//      fn_read --> Fonction de lecture de la fréquence (retourne des Hertz)
 // Test tout les filtres et retient le plus lumineux
-///La fonction hzToColor reçoit en argument les 4 pins de contrôle du capteur couleur ainsi qu'une fonction de lecture de la fréquence. Elle retourne un struct Color.
-pub fn hzToColor<S1,S2,S3,S4>(s0 : &mut S1, s1 : &mut S2, s2: &mut S3, s3: &mut S4, fnRead: &Fn() -> u32) -> Color
+///La fonction hzToColor reçoit en argument les 4 pins de contrôle du capteur couleur ainsi qu'une fonction de lecture de la fréquence. Elle retourne un struct color.
+pub fn hz_To_Color<S1,S2,S3,S4>(s0 : &mut S1, s1 : &mut S2, s2: &mut S3, s3: &mut S4, fn_read: &Fn() -> u32) -> color
     where
         S1 : OutputPin,
         S2 : OutputPin,
         S3 : OutputPin,
         S4 : OutputPin
 {
-    let mut color : Color = Color {
-        Red: 0,
-        Green: 0,
-        Blue: 0
+    let mut couleur : color = color {
+        red: 0,
+        green: 0,
+        blue: 0
     };
-    let mut freqMax : u32;
+    let freq_max : u32;
 
-    color.Green = 0;
-    color.Blue = 0;
-    color.Red = 0;
+    couleur.green = 0;
+    couleur.blue = 0;
+    couleur.red = 0;
 
     s0.set_high();
     s1.set_high();
@@ -70,29 +70,29 @@ pub fn hzToColor<S1,S2,S3,S4>(s0 : &mut S1, s1 : &mut S2, s2: &mut S3, s3: &mut 
     //On lit les blancs
     s2.set_high();
     s3.set_low();
-    freqMax = fnRead();
+    freq_max = fn_read();
 
     //On lis le vert
     s2.set_high();
     s3.set_high();
-    color.Green = ((fnRead() as f32/freqMax as f32)*255.0) as u32 ;
+    couleur.green = ((fn_read() as f32/freq_max as f32)*255.0) as u32 ;
 
     //On lis le bleu
     s2.set_low();
     s3.set_high();
-    color.Blue = ((fnRead() as f32/freqMax as f32)*255.0) as u32;
+    couleur.blue = ((fn_read() as f32/freq_max as f32)*255.0) as u32;
 
     //On lis le rouge
     s2.set_low();
     s3.set_low();
-    color.Red = ((fnRead() as f32/freqMax as f32)*255.0) as u32;
+    couleur.red = ((fn_read() as f32/freq_max as f32)*255.0) as u32;
 
     s0.set_low();
     s1.set_low();
 
 
 
-    color
+    couleur
 }
 
 
