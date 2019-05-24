d7ddd52382f9440b85b56322950e3b5c15727609
gasc <mayeul.gasc@orange.fr>
2019-05-24 16:16:48 +0200

    code cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ use embedded_hal::digital::OutputPin;
 //
 
 ///Contient les 3 composantes d'une couleur échantillonées de 0 (noir) à 255 (blanc)
-pub struct color {
+pub struct Color {
     pub red : u32, // de 0 à 255
     pub green : u32,
     pub blue : u32,
@@ -47,14 +47,14 @@ pub struct Hertz(u32);
 // Test tout les filtres et retient le plus lumineux
 ///La fonction hzToColor reçoit en argument les 4 pins de contrôle du capteur couleur, une fonction de lecture de la fréquence ainsi que le nombre de lectures souhaité.
 ///Elle retourne un struct color.
-pub fn hz_To_Color<S1,S2,S3,S4>(s0 : &mut S1, s1 : &mut S2, s2: &mut S3, s3: &mut S4, fn_read: &Fn() -> u32, nb_pass : u32) -> color
+pub fn hz_to_color<S1,S2,S3,S4>(s0 : &mut S1, s1 : &mut S2, s2: &mut S3, s3: &mut S4, fn_read: &Fn() -> u32, nb_pass : u32) -> Color
     where
         S1 : OutputPin,
         S2 : OutputPin,
         S3 : OutputPin,
         S4 : OutputPin
 {
-    let mut couleur : color = color {
+    let mut couleur : Color = Color {
         red: 0,
         green: 0,
         blue: 0
@@ -67,7 +67,7 @@ pub fn hz_To_Color<S1,S2,S3,S4>(s0 : &mut S1, s1 : &mut S2, s2: &mut S3, s3: &mu
     couleur.red = 0;
 
 
-    for i in (1..nb_pass){
+    for _i in 1..nb_pass{
 
         s0.set_high();
         s1.set_high();
@@ -80,26 +80,26 @@ pub fn hz_To_Color<S1,S2,S3,S4>(s0 : &mut S1, s1 : &mut S2, s2: &mut S3, s3: &mu
         //On lis le vert
         s2.set_high();
         s3.set_high();
-        couleur.green = ((fn_read() as f32/freq_max as f32)*255.0) as u32 + couleur.green ;
+        couleur.green += ((fn_read() as f32/freq_max as f32)*255.0) as u32;
 
         //On lis le bleu
         s2.set_low();
         s3.set_high();
-        couleur.blue = ((fn_read() as f32/freq_max as f32)*255.0) as u32 + couleur.blue;
+        couleur.blue += ((fn_read() as f32/freq_max as f32)*255.0) as u32;
 
         //On lis le rouge
         s2.set_low();
         s3.set_low();
-        couleur.red = ((fn_read() as f32/freq_max as f32)*255.0) as u32 + couleur.red;
+        couleur.red += ((fn_read() as f32/freq_max as f32)*255.0) as u32;
 
         s0.set_low();
         s1.set_low();
 
     }
 
-    couleur.blue = couleur.blue / nb_pass;
-    couleur.green = couleur.green / nb_pass;
-    couleur.red = couleur.red / nb_pass;
+    couleur.blue /= nb_pass;
+    couleur.green /= nb_pass;
+    couleur.red /= nb_pass;
 
     couleur
 }
